d9b2d68b6a1dd89036ec10833c4192f1cad89810
seabelle mcfarlane <seabellemcfarlane@aol.com>
2018-01-20 16:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+happyHolidays()
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+happyHolidayTo(happyHolidays, name)
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+//happyHolidayTo(Happy, name)
+function holidayCountdown(holiday, number) {
+  return `It\'s ${number} days until ${holiday}!`;
+}
